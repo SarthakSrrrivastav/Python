b6acb21cd2b31559c0505e5c83c0f1c4ce709b50
Sarthak Srivastava <74153379+SarthakSrrrivastav@users.noreply.github.com>
2022-10-28 14:15:27 +0530

    Add files via upload

---
+++ list_slicing.py
@@ -0,0 +1,14 @@
+List=[0,1,2,3,4,5,6,7,8,9]
+
+
+
+print(List[2::])  # from 2nd index to last
+print("----------------------------------------")
+
+print(List[0:6])  # from  0 index to 5th index
+print("----------------------------------------")
+
+print(List[2::2])  # from 2nd index to last increment by 1
+print("----------------------------------------")
+
+print(List[::2])  # from starting to last by increment by 2
